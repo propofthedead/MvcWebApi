f52f7754cc8d297998f497552ca2c1ff8b05834a
Foster Stule <camodude5@yahoo.com>
2018-06-21 15:57:13 -0400

    added some more methods

--- MVCWebApiTutorial/Controllers/CustomersController.cs
+++ MVCWebApiTutorial/Controllers/CustomersController.cs
@@ -26,5 +26,43 @@ namespace MVCWebApiTutorial.Controllers
 			}
 			return db.Customers.Find(id);
 		}
+		[HttpPost]
+		[ActionName("Create")]
+		public bool Create(Customer customer) {
+			if (customer == null)
+				return false;
+			if (!ModelState.IsValid)
+				return false;
+			db.Customers.Add(customer);
+			db.SaveChanges();
+			return true;
+		}
+		[HttpPost]
+		[ActionName("Change")]
+		public bool Change(Customer customer) {
+			if (customer == null)
+				return false;
+			if (!ModelState.IsValid)
+				return false;
+			var cust = db.Customers.Find(customer.Id);
+			cust.Name = customer.Name;
+			cust.City = customer.City;
+			cust.State = customer.State;
+			cust.Preferred = customer.Preferred;
+			db.SaveChanges();
+			return true;
+		}
+		[HttpPost]
+		[ActionName("Remove")]
+		public bool Remove(Customer customer) {
+			if (customer == null)
+				return false;
+			if (!ModelState.IsValid)
+				return false;
+			var cust = db.Customers.Find(customer.Id);
+			db.Customers.Remove(cust);
+			db.SaveChanges();
+			return true;
+		}
 	}
 }
